4cd294af9f10bdc92502eba29676fd7262738db0
DaCurse <dacurse0@gmail.com>
2020-12-01 12:00:07 +0200

    Added Day 1 Part two solution and tests

--- src/solutions/2020/aoc01.js
+++ src/solutions/2020/aoc01.js
@@ -1,7 +1,9 @@
+// Time complexity: O(n)? Depends on the implementation of `Set`
+// Space complexity: O(n) We use a set to store previous numbers
 export function partOne(inputs) {
+  const target = 2020;
   // We will store numbers in a set until we find our solution
   const prev = new Set();
-  const target = 2020;
 
   for (let i = 0; i < inputs.length; ++i) {
     const input = inputs[i];
@@ -16,6 +18,35 @@ export function partOne(inputs) {
         fluff: [target - input, input],
         result: (target - input) * input,
       };
+    }
+  }
+}
+
+// Time complexity: O(n^2) Two nested loops going over the input
+// Space complexity: O(1) No extra space is used
+export function partTwo(inputs) {
+  const target = 2020;
+  // Quick sort numbers
+  inputs.sort();
+
+  for (let i = 0; i < inputs.length; ++i) {
+    // We start adding elements from the corners, moving right if the sum is
+    // too small or left if the sum is too large
+    let left = i + 1;
+    let right = inputs.length - 1;
+
+    while (left < right) {
+      const sum = inputs[i] + inputs[left] + inputs[right];
+      if (sum === target) {
+        return {
+          fluff: [inputs[i], inputs[left], inputs[right]],
+          result: inputs[i] * inputs[left] * inputs[right],
+        };
+      } else if (sum <= target) {
+        left++;
+      } else {
+        right--;
+      }
     }
   }
 }
--- src/tests/2020/aoc01.spec.js
+++ src/tests/2020/aoc01.spec.js
@@ -1,14 +1,24 @@
-import { partOne } from '../../solutions/2020/aoc01';
+import { partOne, partTwo } from '../../solutions/2020/aoc01';
 import getInputs from '../../util/inputs';
 
 describe('Day 1 solution', () => {
-  it('Should find a pair that sums to 2020', async () => {
+  test('Part one: Find a pair that sums to 2020', async () => {
     const inputs = await getInputs(1);
     const { fluff, result } = partOne(inputs);
-    console.log('Day 1 solution:', result);
+    console.log('Day 1 Part 1 solution:', result);
 
     expect(inputs).toEqual(expect.arrayContaining(fluff));
     expect(fluff[0] + fluff[1]).toEqual(2020);
     expect(fluff[0] * fluff[1]).toEqual(result);
+  });
+
+  test('Part two: Find a triplet that sums to 2020', async () => {
+    const inputs = await getInputs(1);
+    const { fluff, result } = partTwo(inputs);
+    console.log('Day 1 Part 2 solution:', result);
+
+    expect(inputs).toEqual(expect.arrayContaining(fluff));
+    expect(fluff[0] + fluff[1] + fluff[2]).toEqual(2020);
+    expect(fluff[0] * fluff[1] * fluff[2]).toEqual(result);
   });
 });
