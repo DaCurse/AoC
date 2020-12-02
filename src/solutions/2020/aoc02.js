517b3caed341ff02c0d32c404a4a1f58573f543f
DaCurse <dacurse0@gmail.com>
2020-12-02 13:17:25 +0200

    Optimize Day 2 solution

--- src/solutions/2020/aoc02.js
+++ src/solutions/2020/aoc02.js
@@ -1,9 +1,9 @@
-const pwRegex = /(\d+)-(\d+)\s(\w):\s(\w+)/;
+const splitRegex = /-|\s|: /;
 
 export function partOne(inputs) {
   return inputs.reduce((acc, input) => {
     // Parse line using regex
-    const [, min, max, letter, pw] = input.match(pwRegex);
+    const [min, max, letter, pw] = input.split(splitRegex);
     // Count occurances
     const count = pw.split(letter).length - 1;
 
@@ -15,11 +15,11 @@ export function partOne(inputs) {
 export function partTwo(inputs) {
   return inputs.reduce((acc, input) => {
     // Parse line using regex
-    const [, pos1, pos2, letter, pw] = input.match(pwRegex);
+    const [pos1, pos2, letter, pw] = input.split(splitRegex);
 
     // Use XOR to determine if only one of the statements is true and incremenet
     // accumulator accordingly
-    return (pw[pos1 - 1] === letter) ^ (pw[pos2 - 1] === letter)
+    return (pw[Number(pos1) - 1] === letter) ^ (pw[Number(pos2) - 1] === letter)
       ? acc + 1
       : acc;
   }, 0);
