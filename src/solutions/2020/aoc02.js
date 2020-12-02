b53cba8c00ec3e36cc553751e77cd6c6976d08e0
DaCurse <dacurse0@gmail.com>
2020-12-02 11:37:42 +0200

    Added Day 2 Part two solution and tests

--- src/solutions/2020/aoc02.js
+++ src/solutions/2020/aoc02.js
@@ -2,9 +2,25 @@ const pwRegex = /(\d+)-(\d+)\s(\w):\s(\w+)/;
 
 export function partOne(inputs) {
   return inputs.reduce((acc, input) => {
+    // Parse line using regex
     const [, min, max, letter, pw] = input.match(pwRegex);
+    // Count occurances
     const count = pw.split(letter).length - 1;
 
+    // Incrementing accumulator if count is within the range
     return count >= Number(min) && count <= Number(max) ? acc + 1 : acc;
+  }, 0);
+}
+
+export function partTwo(inputs) {
+  return inputs.reduce((acc, input) => {
+    // Parse line using regex
+    const [, pos1, pos2, letter, pw] = input.match(pwRegex);
+
+    // Use XOR to determine if only one of the statements is true and incremenet
+    // accumulator accordingly
+    return (pw[pos1 - 1] === letter) ^ (pw[pos2 - 1] === letter)
+      ? acc + 1
+      : acc;
   }, 0);
 }
--- src/tests/2020/aoc02.spec.js
+++ src/tests/2020/aoc02.spec.js
@@ -1,12 +1,20 @@
-import { partOne } from '../../solutions/2020/aoc02';
+import { partOne, partTwo } from '../../solutions/2020/aoc02';
 import getInputs from '../../util/inputs';
 import { parseListOfStrings } from '../../util/parsers';
 
 describe('Day 2 solution', () => {
-  test('Part one', async () => {
+  test('Part one: Find all passwords where a letter occurs within a given range', async () => {
     const inputs = await getInputs(2, parseListOfStrings);
     const result = partOne(inputs);
     console.log('Day 2 Part 1 solution:', result);
+
+    expect(result).toMatchSnapshot();
+  });
+
+  test('Part two: Find all passwords where a letter is only in one of 2 given positions', async () => {
+    const inputs = await getInputs(2, parseListOfStrings);
+    const result = partTwo(inputs);
+    console.log('Day 2 Part 2 solution:', result);
 
     expect(result).toMatchSnapshot();
   });
