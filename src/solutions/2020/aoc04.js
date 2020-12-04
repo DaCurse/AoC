43c806b27d7089f0c7d8ac4437b5aeac2103b325
DaCurse <dacurse0@gmail.com>
2020-12-04 11:27:36 +0200

    Added Day 4 Part two solution and tests

--- src/solutions/2020/aoc04.js
+++ src/solutions/2020/aoc04.js
@@ -1,12 +1,79 @@
-export function parseFields(raw) {
-  return raw.split('\n\n').map((p) => p.split(/\s|:/));
+/**
+ * This abomination parses a list of passports (huge string) seperated by blank lines into objects with keys/values
+ * being each field and it's respective value
+ * @param {string} raw Raw input (List of passports)
+ */
+export function parsePassports(raw) {
+  return raw.split('\n\n').map((passport) => {
+    const parts = passport.split(/\s/);
+    return parts.reduce((obj, part) => {
+      const [key, value] = part.split(':');
+      if (key === 'cid' || key.length === 0) return obj;
+      return { ...obj, [key]: value };
+    }, {});
+  });
+}
+
+/**
+ * Checks if a passport has all required fields
+ * @param {string[]} fields Array of fields
+ */
+function hasAllFields(fields) {
+  const requiredFields = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid'];
+  return requiredFields.every((field) => fields.includes(field));
 }
 
 export function partOne(inputs) {
-  const requiredFields = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid'];
-  return inputs.reduce(
-    (acc, passport) =>
-      requiredFields.every((field) => passport.includes(field)) ? acc + 1 : acc,
-    0
-  );
+  // Return the count of all passports with all required fields
+  return inputs.filter((passport) => hasAllFields(Object.keys(passport)))
+    .length;
+}
+
+/**
+ * Utility function to convert `value` to String and check if it's within a
+ * certain range
+ * @param {string} value
+ * @param {number} min
+ * @param {number} max
+ */
+function between(value, min, max) {
+  return parseInt(value) >= min && parseInt(value) <= max;
+}
+
+// Key/value pairs where the key is the field and the value is a function which check if it's valid
+const predicates = {
+  byr(value) {
+    return between(value, 1920, 2002);
+  },
+  iyr(value) {
+    return between(value, 2010, 2020);
+  },
+  eyr(value) {
+    return between(value, 2020, 2030);
+  },
+  hgt(value) {
+    return value.endsWith('cm')
+      ? between(value, 150, 193)
+      : value.endsWith('in')
+      ? between(value, 59, 76)
+      : false;
+  },
+  hcl(value) {
+    return /^#[0-9a-f]{6}$/.test(value);
+  },
+  ecl(value) {
+    return ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'].includes(value);
+  },
+  pid(value) {
+    return /^[0-9]{9}$/.test(value);
+  },
+};
+
+export function partTwo(inputs) {
+  // Return the count of all passports that have all required fields and field values pass all the predicates
+  return inputs.filter(
+    (passport) =>
+      hasAllFields(Object.keys(passport)) &&
+      Object.keys(passport).every((field) => predicates[field](passport[field]))
+  ).length;
 }
--- src/tests/2020/aoc04.spec.js
+++ src/tests/2020/aoc04.spec.js
@@ -1,11 +1,19 @@
-import { parseFields, partOne } from '../../solutions/2020/aoc04';
+import { parsePassports, partOne, partTwo } from '../../solutions/2020/aoc04';
 import getInputs from '../../util/inputs';
 
 describe('Day 4 solution', () => {
-  test('Part one: Count all passports with at least 8 fields', async () => {
-    const inputs = await getInputs(4, parseFields);
+  test('Part one: Count all passports with at least 7 fields', async () => {
+    const inputs = await getInputs(4, parsePassports);
     const result = partOne(inputs);
     console.log('Day 4 Part 1 solution:', result);
+
+    expect(result).toMatchSnapshot();
+  });
+
+  test('Part two: Count all passports with valid fields and field values', async () => {
+    const inputs = await getInputs(4, parsePassports);
+    const result = partTwo(inputs);
+    console.log('Day 4 Part 2 solution:', result);
 
     expect(result).toMatchSnapshot();
   });
