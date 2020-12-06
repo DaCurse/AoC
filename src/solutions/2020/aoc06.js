8257df4a1f4a8158bf271236c69968300ece002e
DaCurse <dacurse0@gmail.com>
2020-12-06 19:58:06 +0200

    Added Day 6 Part two solution

--- src/solutions/2020/aoc06.js
+++ src/solutions/2020/aoc06.js
@@ -1,6 +1,19 @@
+import { parseListOfStrings } from '../../util/parsers';
+
 export function partOne(inputs) {
   return inputs.reduce(
     (total, input) => total + new Set(input.replace(/\s/g, '')).size,
     0
   );
+}
+
+export function partTwo(inputs) {
+  return inputs.reduce((total, input) => {
+    const unanimous = parseListOfStrings(input)
+      .map((s) => Array.from(s))
+      .reduce((intersection, answers) =>
+        intersection.filter((a) => answers.includes(a))
+      );
+    return total + unanimous.length;
+  }, 0);
 }
--- src/tests/2020/aoc06.spec.js
+++ src/tests/2020/aoc06.spec.js
@@ -1,4 +1,4 @@
-import { partOne } from '../../solutions/2020/aoc06';
+import { partOne, partTwo } from '../../solutions/2020/aoc06';
 import getInputs from '../../util/inputs';
 import { parseListOfGroups } from '../../util/parsers';
 
@@ -7,6 +7,14 @@ describe('Day 6 solution', () => {
     const inputs = await getInputs(6, parseListOfGroups);
     const result = partOne(inputs);
     console.log('Day 6 Part 1 solution:', result);
+
+    expect(result).toMatchSnapshot();
+  });
+
+  test('Part one: Find the sum of unanimous questions for each group', async () => {
+    const inputs = await getInputs(6, parseListOfGroups);
+    const result = partTwo(inputs);
+    console.log('Day 6 Part 2 solution:', result);
 
     expect(result).toMatchSnapshot();
   });
